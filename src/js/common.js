e35a332fc7fc472dc06302c1a7dd67ed3eab5b74
Jane Des <desevp@gmail.com>
2018-10-14 18:56:12 +0300

    Added layout and animation

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   'use strict';
+  @@include('partials/video-stream.js')
   @@include('partials/main.js')
+
 });
 
 window.onload = function() {
